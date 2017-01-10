25fce8d9d1244c2284f266106e30719fa97f6e8b
kenkoii <jankenith.johannessen@gmail.com>
2017-01-10 10:51:59 +0800

    fixed nav bar

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
-import { FirebaseAuth, FirebaseAuthState } from 'angularfire2';
-import {Router} from '@angular/router';
+import { FirebaseAuth, FirebaseAuthState, FirebaseListObservable } from 'angularfire2';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class UserService {
   public UserAuthState: FirebaseAuthState;
-  public User: any;
+  public user: any;
   constructor(private http: Http, public af: AngularFire,  private router: Router) {
     this.af.auth.subscribe(
               auth => {
                 this.UserAuthState = auth;
-                if(auth!=null){
-                  this.router.navigateByUrl('admin');
-                }
-                console.log(auth);
               },
               err => console.log(err));
   }
@@ -27,17 +23,37 @@ export class UserService {
           },{
           provider: AuthProviders.Password,
           method: AuthMethods.Password,
+        }).then((data)=>{
+            this.af.database.list('/users/', {
+              query: {
+                orderByChild: 'email_address',
+                equalTo: data.auth.email
+              }
+            }).subscribe( res => {
+              this.user = res[0];
+              localStorage.setItem('role',this.user.type)
+              console.log(this.user);
+              if(this.user.type == 'admin'){
+                this.router.navigateByUrl('admin');
+              }else{
+                this.router.navigateByUrl('');
+              }
+            });
+        }).catch((err)=>{
+          console.log(err);
         });
   }
 
   userLogout(){
     this.af.auth.logout();
+    localStorage.removeItem('role');
   }
 
   isLoggedIn(){
     return this.UserAuthState?true:false;
   }
 
-
-
+  getRole(){
+    return localStorage.getItem('role') || ''
+  }
 }
